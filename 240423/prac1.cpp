784b4bcd6b11450820bea61c82625df2400a4968
seoyoung0221 <jjan000221@gmail.com>
2024-04-23 17:06:14 +0900

    prac1

--- 240423/prac1.cpp
+++ 240423/prac1.cpp
@@ -2,9 +2,20 @@
 
 int main()
 {
-	for (int i = 0; i < 5; i++)
+	std::cout << "5단 출력\n";
+	for (int i = 1; i <= 9; i++)
 	{
-		std::cout << "Iteration : " << i + 1 << std::endl;
+		std::cout << "5 * " << i << "= " << 5 * i << "\n";
+	}
+
+	std::cout << "구구단 출력\n";
+	for (int i = 1; i <= 9; i++)
+	{
+		std::cout << i << "단 출력\n";
+		for (int j = 1; j <= 9; j++)
+		{
+			std::cout << i << " * " << j << " = " << i * j << "\n";
+		}
 	}
 
 	return 0;
